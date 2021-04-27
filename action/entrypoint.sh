e8ff738f7b1cb82b1681ad014371eeff60ded808
elin <eryman@kth.se>
2021-04-27 10:41:56 +0200

    testing

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 echo "Script executed from: ${PWD}"
-echo "Files are: ${LS}"
+echo "Files are: ${ls}"
 
-output=$(python3 group-of-three.py $1 $2 $3 $4)
+output=$(python3 action/group-of-three.py $1 $2 $3 $4)
 echo $output
