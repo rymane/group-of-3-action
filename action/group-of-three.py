cb0c90f681ae24cc3c3d38697f2bf21dd2815eb4
Agnes <agnesfo@kth.se>
2021-04-27 22:35:25 +0200

    test

--- action/group-of-three.py
+++ action/group-of-three.py
@@ -14,11 +14,38 @@ def process_json(payload):
     return branch_main, branch_head, repo_main, repo_head, pull_request_number
     
 
-def process_added_files(files_added):
+def process_added_files(files):
     file_parts = []
-    for f in files_added:
+    for f in files:
         file_parts.append(f.split('/'))
     return file_parts
+
+def check_student_pr(files):
+    valid = True
+    valid_files = []
+    valid_tasks = ['course-automation', 'demo', 'essay', 'executable-tutorial', 'feedback', 'open-source', 'presentation']
+    task = ""
+    num_students = -1
+    for f in files:
+        if not len(f) >= 4:
+            valid = False
+        if f[0] != 'contributions':
+            valid = False
+        if f[1] not in valid_tasks:
+            valid = False
+        if not 1 <= len(f[2].split('-')) <= 3:
+            valid = False
+        valid_files.append(valid)
+        valid = True
+
+    for i, vf in valid_files:
+        # get task and number of studens from first valid changed/added file
+        if vf == True:
+            task = files[i][1]
+            num_students = len(files[i][2].split('-'))
+            break
+
+    return valid_files, task, num_students
 
 def main():
     github_token = sys.argv[1]
@@ -28,6 +55,12 @@ def main():
 
     file_added_parts = process_added_files(files_added)
     file_changed_parts = process_added_files(files_changed)
+
+    files_parts = file_added_parts
+    for f in file_changed_parts:
+        files_parts.append(f)
+
+    valid_files, task, num_students = check_student_pr(files_parts)
 
     branch_main, branch_head, repo_main, repo_head, pull_request_number = process_json(payload)
 
@@ -40,7 +73,10 @@ def main():
         "head" : branch_head,
         "main-repo": repo_main,
         "head-repo": repo_head,
-        "pr-num" : pull_request_number
+        "pr-num" : pull_request_number,
+        "valid_files" : valid_files,
+        "task" : task,
+        "num_students": num_students
     })) 
     #with open('payload.json') as f:
     
