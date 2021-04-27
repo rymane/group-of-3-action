229bc92f0ab809dc2ecef0326961402390aed772
elin <eryman@kth.se>
2021-04-27 14:55:52 +0200

    test   entrypoint

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/sh -l
 
-output=$(python3 action/group-of-three.py "$1" "$2" "$3" "$4")
-
-output="${output//'%'/'%25'}"
-output="${output//$'\n'/'%0A'}"
-output="${output//$'\r'/'%0D'}"
-
-echo "heeeeeeeej"
-echo $output >> output.output
+echo "heeeeeeeeeeeeeej"
