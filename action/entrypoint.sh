d7a4519f9c7b1313e46e443afb6a5b8122097d57
elin <eryman@kth.se>
2021-04-27 10:25:44 +0200

    fix path

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -2,5 +2,5 @@
 echo "Script executed from: ${PWD}"
 echo "Files are: ${LS}"
 
-output= $(python3 group-of-three.py "$1" "$2" "$3" "$4")
+output= $(python3 ../../action/group-of-three.py "$1" "$2" "$3" "$4")
 echo output
