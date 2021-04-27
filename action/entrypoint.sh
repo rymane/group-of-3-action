c1ac42c330c813b523191eb777c5b20d721ec7cc
elin <eryman@kth.se>
2021-04-27 13:16:41 +0200

    process json

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh -l
-echo "Script executed from: ${PWD}"
-echo ${ls}
 
 output=$(python3 action/group-of-three.py $1 $2 $3 $4)
 echo $output
--- action/group-of-three.py
+++ action/group-of-three.py
@@ -1,10 +1,15 @@
 import sys
+import json
 
+def process_json(payload):
+    python_obj = json.load(payload)
+    print(python_obj)
+    
+def main():
+    github_token = sys.argv[1]
+    payload = sys.argv[2]
+    files_added = sys.argv[3]
+    files_changed = sys.argv[4]
+    process_json(payload)
 
-def test():
-    print(sys.argv[1])
-    print(sys.argv[2])
-    print(sys.argv[3])
-
-
-test()
+main()
