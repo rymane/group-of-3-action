b7dd6951864778ce21dba40f941c45315834c9dc
elin <eryman@kth.se>
2021-04-28 11:15:33 +0200

    clean up, comments added

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,5 +1,3 @@
 json_output=$(python3 action/group-of-three.py "$1" "$2" "$3" "$4")
 
-ls
-echo "heeeeeeeej"
 echo $json_output
--- action/group-of-three.py
+++ action/group-of-three.py
@@ -4,7 +4,7 @@ import re
 
 def process_json(payload):
     python_obj = json.loads(payload)
-    # fetch main branch, head branch, main repo and repo head
+    # fetch main branch and repo, head branch and repo (forked)
     branch_main = python_obj['pull_request']['base']['ref']
     branch_head = python_obj['pull_request']['head']['ref']
     repo_main = python_obj['pull_request']['base']['repo']['full_name']
@@ -13,13 +13,18 @@ def process_json(payload):
 
     return branch_main, branch_head, repo_main, repo_head, pull_request_number
     
-
+# Split added and changed files
 def process_added_files(files):
     file_parts = []
     for f in files:
         file_parts.append(f.split('/'))
     return file_parts
 
+"""
+Check PR to see if it is a course contribution with a path "/contributions/ .." 
+and that PR is for a valid task e.g. demo, essay etc. 
+Lastly, the number of students contributed to the PR is extracted.  
+"""
 def check_student_pr(files):
     valid = True
     valid_files = []
@@ -46,6 +51,22 @@ def check_student_pr(files):
 
     return valid_files, task, student_names,num_students
 
+def group_of_three(task, num_students):
+    valid_task_three = ['essay', 'demo', 'open-source']
+    valid = True
+    if num_students == 3:
+        if task not in valid_task_three:
+            valid = False
+
+    return valid
+
+""" 
+Process the expected input from command line:
+- Guthub  token
+- Payload from pull request
+- List of filepaths for files added 
+- List of filepaths for files changed
+"""
 def main():
     github_token = sys.argv[1]
     payload = sys.argv[2]
@@ -55,16 +76,25 @@ def main():
     file_added_parts = process_added_files(files_added)
     file_changed_parts = process_added_files(files_changed)
 
+    # append added files and changed files into one list.
     files_parts = file_added_parts
     for f in file_changed_parts:
         files_parts.append(f)
 
+
     valid_files, task, student_names, num_students = check_student_pr(files_parts)
+
+    if num_students == 2:
+        # Produce the content for commenting on PR.
+        print(" ")
+    elif num_students == 3:
+        valid_group_of_three = group_of_three(task, num_students)
+        # Produce the content for commenting on PR.
 
     branch_main, branch_head, repo_main, repo_head, pull_request_number = process_json(payload)
 
     print(json.dumps({
-        "file-parts": file_added_parts,
+        "file_added_parts": file_added_parts,
         "files_added" : files_added,
         "files_changed" : files_changed,
         "files_changed_parts" : file_changed_parts,
@@ -77,13 +107,8 @@ def main():
         "task" : task,
         "student_names": student_names,
         "num_students": num_students
-    })) 
-    #with open('payload.json') as f:
-    
+    }))
 
-    #process_json(payload)
-
-    
 
 if __name__ == "__main__":
     main()
