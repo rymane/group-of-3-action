68644fc1599a64aee150da04ef98eaf98a3e5bcc
elin <eryman@kth.se>
2021-04-28 15:27:24 +0200

    test

--- action/group-of-three.py
+++ action/group-of-three.py
@@ -94,7 +94,7 @@ def main():
     file_added_parts = process_added_files(files_added)
     file_changed_parts = process_added_files(files_changed)
 
-    test = sys.argv[5]
+    test = sys.argv[5].split('/')
     # append added files and changed files into one list.
     files_parts = file_added_parts
     for f in file_changed_parts:
